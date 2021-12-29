3b9f574a538a034eadc7dcb86ed65742da1c05cb
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-29 21:41:59 +0100

    feat: add landing page

---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import styles from "./index.module.css";
+import HomepageFeatures from "../components/HomepageFeatures";
+
+function HomepageHeader() {
+	return (
+		<header className={clsx("hero hero--dark-blue", styles.heroBanner)}>
+			<div className="container">
+				<img src="img/favicon.ico" />
+				<h1 className="hero__title">PaperPlane</h1>
+				<h2>Fast file/shorturl upload server packed with lots of cool features!</h2>
+				<div className={styles.buttons}>
+					<Link className="button button--secondary button--lg" to="/docs/get-started">
+						Get Started
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
+}
+
+export default function Home() {
+	return (
+		<Layout
+			// title={`${siteConfig.title}`}
+			description="Fast file/shorturl upload server packed with lots of cool features!">
+			<HomepageHeader />
+			<main>
+				<HomepageFeatures />
+			</main>
+		</Layout>
+	);
+}
